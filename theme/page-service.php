4057ba8384d75f7bcec07075c15a6758c0b3df87
Steve Reyes <steve@example.com>
2026-06-17 20:50:56 +0000

    fix: properly rendered lightning bolt favicon (svg+ico) with rsvg

--- theme/page-service.php
+++ theme/page-service.php
@@ -8,6 +8,7 @@
 <head>
   <meta charset="UTF-8">
   <link rel="icon" type="image/x-icon" href="/favicon.ico">
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg">
   <link rel="apple-touch-icon" href="/wp-content/themes/nimo/img/apple-touch-icon.png">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php wp_title('|', true, 'right'); ?> LeadZap — Services</title>
